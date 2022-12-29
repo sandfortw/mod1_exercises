0358c7da1c12bbc1c68d942a6f672165deb21dae
Weston Sandfort <sandfortw@gmail.com>
2022-12-29 12:54:02 -0700

    Add max and min pattern

--- iteration/exercises/spec/max_and_min_by_pattern_spec.rb
+++ iteration/exercises/spec/max_and_min_by_pattern_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'max and min by pattern' do
   it 'test 1'  do
-    numbers = [1, 100, 1000, 1000000]
+    numbers = [1, 100, 1000, 100000]
     greatest = numbers[0]
     numbers.each do |number|
       if number > greatest
@@ -26,17 +26,19 @@ RSpec.describe 'max and min by pattern' do
     expect(greatest).to eq(1000000)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     meals = ["banana", "nuts", "salad", "steak", "cake"]
     shortest_word = meals[0]
     meals.each do |meal|
-      # Your Code Here
+      if meal.length < shortest_word.length
+        shortest_word = meal
+      end
     end
 
     expect(shortest_word).to eq("nuts")
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     meals = {
       breakfast: "banana",
       snack: "nuts",
@@ -46,21 +48,27 @@ RSpec.describe 'max and min by pattern' do
     }
     shortest_word = meals[meals.keys.first]
     meals.each do |meal, dish|
-      # Your Code Here
+      if dish.length < shortest_word.length
+        shortest_word = meals[meal]
+      end
     end
 
     expect(shortest_word).to eq("nuts")
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     stats = [3001, 431, 1695, 0.27601, 0.340]
     most_digits = stats[0]
-    # Your Code Here
+    stats.each do |stat|
+      if stat.to_s.length > most_digits.to_s.length
+        most_digits = stat
+      end
+    end
 
     expect(most_digits).to eq(0.27601)
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     stats = {
       games_played: 3001,
       home_runs: 431,
@@ -69,19 +77,28 @@ RSpec.describe 'max and min by pattern' do
       on_base_percentage: 0.340
     }
     most_digits = stats[stats.keys.first]
-    # Your Code Here
+    stats.each do |key, value|
+      if stats[key].to_s.length > most_digits.to_s.gsub(".", "").length
+        most_digits = value
+      end
+    end
 
     expect(most_digits).to eq(0.27601)
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     ages = [39, 45, 29, 24, 50]
-    # Your Code Here
+    oldest = ages.first
+    ages.each do |age|
+      if age > oldest
+        oldest = age
+      end
+    end
 
     expect(oldest).to eq(50)
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     ages = {
       abdi: 39,
       hassan: 45,
@@ -89,22 +106,38 @@ RSpec.describe 'max and min by pattern' do
       margaret: 24,
       miguel: 50
     }
-    # Your Code Here
+    oldest = {:name => ages.keys.first, :age => ages.values.first}
+    ages.each do | name, age |
+      if age > oldest[:age]
+        oldest ={:name => name.to_s, :age => age}
+      end
+    end
 
     expected = {name: "miguel", age: 50}
     expect(oldest).to eq(expected)
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     programmers = [["katrina", "sandi", "jim", "aaron", "desi"], ["abby", "jon", "susan"]]
-    # Your Code Here
+    fewest_programmers = programmers[0]
+    programmers.each do |list|
+      if list.size < fewest_programmers.size
+        fewest_programmers = list
+      end
+    end
 
     expect(fewest_programmers).to eq(["abby", "jon", "susan"])
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     programmers = {ruby: ["katrina", "sandi", "jim", "aaron", "desi"], java: ["abby", "jon", "susan"]}
     # Your Code Here
+    fewest_programmers = programmers.keys.first
+    programmers.each do |key, value|
+      if value.size < programmers[fewest_programmers].size
+        fewest_programmers = key
+      end
+    end
 
     expect(fewest_programmers).to eq(:java)
   end
